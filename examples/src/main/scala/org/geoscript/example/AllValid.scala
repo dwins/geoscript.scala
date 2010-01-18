d52384bd4c6a31c79b540112005a0c065180a123
David Winslow <cdwinslow@gmail.com>
2010-01-18 17:26:21 -0500

    Update examples to reflect new API structure.

--- examples/src/main/scala/org/geoscript/example/AllValid.scala
+++ examples/src/main/scala/org/geoscript/example/AllValid.scala
@@ -1,26 +1,15 @@
 package org.geoscript.example
 
-import scala.collection.jcl.HashMap
-import com.vividsolutions.jts.geom.Geometry
-import org.geotools.data.DataStoreFinder
-import org.opengis.feature.simple.SimpleFeature
+import org.geoscript._
 
-object AllValid extends org.geoscript.feature.GeoCrunch {
+object AllValid extends GeoScript with feature.GeoCrunch {
   def main(args: Array[String]) = {
-    val file = promptShapeFile
-    val params = (new HashMap + ("url" -> file.toURI.toURL)).underlying
-    val dataStore = DataStoreFinder.getDataStore(params)
-    val typeName = dataStore.getTypeNames()(0)
-    val featureSource = dataStore.getFeatureSource(typeName)
-
-    var invalid = Nil: List[SimpleFeature]
-
-    foreach (featureSource.getFeatures) { feature => 
-      if (!feature.getDefaultGeometry.asInstanceOf[Geometry].isValid) 
-        invalid ::= feature
-    }
+    val file = promptShapeFile.toURI.toString
+    val shp = layer.Shapefile(file)
+  
+    val invalid = shp.features filter { f => !f.geometry.isValid } toSeq
     
-    println("Found %1$s invalid features.  That's %1$s too many.".format(invalid.length))
-    invalid.foreach(x => println(x.getID))
+    println("Found %s invalid features.".format(invalid.size))
+    for (f <- invalid) println(f.id)
   }
 }
