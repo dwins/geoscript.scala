aa797c0cf430f3b8a90da3b7361d1488723364e9
David Winslow <cdwinslow@gmail.com>
2010-01-18 10:52:48 -0500

    Formatting tweaks in geometry tests

--- geoscript/src/test/scala/org/geoscript/UsageTests.scala
+++ geoscript/src/test/scala/org/geoscript/UsageTests.scala
@@ -9,26 +9,32 @@ import layer._
 import projection._
 
 class UsageTests extends Specification with GeoScript {
-  "geometries" should  { 
+  "geometries" should { 
     "work like on the geoscript homepage" in { 
-    var p = Point(-111, 45.7)
-    var p2 = Projection("epsg:4326") to Projection("epsg:26912") apply p
-    var poly = p.buffer(100)
-    
-    p2.x must beCloseTo(499999.0, 0.5)
-    p2.y must beCloseTo(5060716.0, 0.5)
-    poly.area must beCloseTo(31214.45, 0.01)
-  }
-  "lineStrings should be easy" in { 
-    LineString((10.0, 10.0), (20.0, 20.0), (30.0, 40.0)) 
-  }
-  "polygon should be easy" in { 
-    Polygon(Seq((10.0, 10.0),(10.0, 20.0),(20.0, 20.0), (20.0, 15.0), (10.0, 10.0)), Seq.empty)
-  }
-  "mulit point should be easy" in {
-    MultiPoint(Seq((20.0,20.0),(10.0,10.0))) 
-  } 
- }  
+      var p = Point(-111, 45.7)
+      var p2 = Projection("epsg:4326") to Projection("epsg:26912") apply p
+      var poly = p.buffer(100)
+
+      p2.x must beCloseTo(499999.0, 0.5)
+      p2.y must beCloseTo(5060716.0, 0.5)
+      poly.area must beCloseTo(31214.45, 0.01)
+    }
+
+    "linestrings should be easy" in { 
+      LineString((10.0, 10.0), (20.0, 20.0), (30.0, 40.0)) 
+    }
+
+    "polygon should be easy" in { 
+      Polygon(
+        Seq((10d, 10d),(10d, 20d),(20d, 20d), (20d, 15d), (10d, 10d)),
+        Seq.empty
+      )
+    }
+
+    "multi point should be easy" in {
+      MultiPoint(Seq((20.0,20.0),(10.0,10.0))) 
+    } 
+  }  
 
   "Layers" should {
     val statesPath = "geoscript/src/test/resources/data/states.shp"
