46fc961d34a894fcdaa49895c959b49f68c05ba7
Ivan Willig <ivan@mao.(none)>
2010-01-22 18:08:44 -0500

    updated MultiPoint to use traits

--- geoscript/src/main/scala/org/geoscript/geometry/MultiPoint.scala
+++ geoscript/src/main/scala/org/geoscript/geometry/MultiPoint.scala
@@ -2,19 +2,36 @@ package org.geoscript.geometry
 
 import com.vividsolutions.jts.{geom=>jts}
 import org.opengis.referencing.crs.CoordinateReferenceSystem
-
 import org.geoscript.projection.Projection
 
 object MultiPoint {
-  import ModuleInternals.factory._
+   class Wrapper(val underlying: jts.MultiPoint) extends MultiPoint {
+    def in(dest: Projection): MultiPoint = new Projected(underlying, dest)
+  }
 
-  def apply(coords: Seq[(Double,Double)]): jts.MultiPoint =
-    createMultiPoint( 
+  class Projected(
+    val underlying: jts.MultiPoint, 
+    override val projection: Projection
+  ) extends MultiPoint {
+    def in(dest: Projection): MultiPoint = 
+      new Projected(projection.to(dest)(underlying), dest)
+  }
+  
+  def apply(mp : jts.MultiPoint): MultiPoint = new Wrapper(mp) 
+
+  def apply(coords: Seq[(Double,Double)]): MultiPoint =
+    new Wrapper(ModuleInternals.factory.createMultiPoint( 
       coords.map ({ elem => new jts.Coordinate(elem._1,elem._2) }).toArray
-    ) 
+    )) 
 }
 
 trait MultiPoint extends Geometry {
   override val underlying: jts.MultiPoint
   override def in(dest: Projection): MultiPoint
+
+  override def transform(dest: Projection): MultiPoint = 
+    MultiPoint(projection.to(dest)(underlying)) in dest
+
+
+
 }
