b62a20e42014b38fd2deaeb4a3df2d6cddbf493a
David Winslow <cdwinslow@gmail.com>
2010-01-23 17:33:10 -0500

    Add some scaladoc comments.

--- geoscript/src/main/scala/org/geoscript/GeoScript.scala
+++ geoscript/src/main/scala/org/geoscript/GeoScript.scala
@@ -1,5 +1,24 @@
 package org.geoscript
 
+/**
+ * The GeoScript trait serves as a convenience for loading all of geoscript
+ * into a compiled Scala program.  Example usage:
+ * <code>
+ *   import org.geoscript._
+ *   object Main extends GeoScript {
+ *     val p = geometry.Point(1, 12);
+ *   }
+ * </code>
+ */
 trait GeoScript extends geometry.Implicits with projection.Implicits
 
+/**
+ * The GeoScript object provides a convenience for loading GeoScript into 
+ * interpreted Scala scripts.  Example usage:
+ * <code>
+ *   import org.geoscript._
+ *   import GeoScript._
+ *   val p = geometry.Point(1, 12);
+ * </code>
+ */
 object GeoScript extends GeoScript
