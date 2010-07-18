48442e627ac5fd101bf4bf6281966ea0c8b5a5d4
David Winslow <cdwinslow@gmail.com>
2010-07-17 23:09:35 -0400

    Determine default image size based on data bounds

--- geoscript/src/main/scala/render/Viewport.scala
+++ geoscript/src/main/scala/render/Viewport.scala
@@ -3,7 +3,6 @@ package org.geoscript
 import collection.JavaConversions._
 import com.vividsolutions.jts.{geom=>jts}
 import java.awt.RenderingHints
-import RenderingHints._
 
 package render {
   class RichImage(private val im: java.awt.image.RenderedImage) {
@@ -33,7 +32,8 @@ package render {
       graphics: java.awt.Graphics2D,
       layers: Seq[(layer.Layer, style.Style)],
       window: java.awt.Rectangle
-    ) = {
+    ) {
+      import RenderingHints._
       val renderer = new org.geotools.renderer.lite.StreamingRenderer()
       renderer.setJava2DHints(new RenderingHints(Map(
         KEY_ANTIALIASING -> VALUE_ANTIALIAS_ON,
@@ -49,14 +49,56 @@ package render {
       renderer.paint(graphics, window, bounds)
     }
 
-    def render(layers: Seq[(layer.Layer, style.Style)]): RichImage = {
+    def render(layers: Seq[(layer.Layer, style.Style)]): RichImage =
+      render(layers, Viewport.frame(layers))
+
+    def render(
+      layers: Seq[(layer.Layer, style.Style)],
+      window: java.awt.Rectangle
+    ): RichImage = {
       import java.awt.image.BufferedImage
-      val image = new BufferedImage(512, 512, BufferedImage.TYPE_INT_ARGB)
+      val image = new BufferedImage(
+        window.width,
+        window.height,
+        BufferedImage.TYPE_INT_ARGB
+      )
       val graphics = image.createGraphics()
-      draw(graphics, layers, new java.awt.Rectangle(0, 0, 512, 512))
+      draw(graphics, layers, window)
       graphics.dispose()
       image
     }
+  }
+
+  object Viewport {
+    /**
+     * From a list of layers and a desired screen size, determine the size of
+     * viewing area that preserves the aspect ratio of the layers' bounds and
+     * fits within the given size.
+     */
+    def frame(
+      layers: Seq[(layer.Layer, style.Style)],
+      maximal: (Int, Int) = (500, 500)
+    ): java.awt.Rectangle = {
+      val realWorldBounds = new jts.Envelope()
+      layers.foreach { l => realWorldBounds.expandToInclude(l._1.bounds) }
+      val aspect = realWorldBounds.getHeight() / realWorldBounds.getWidth()
+      val idealAspect = maximal._2.toDouble / maximal._1.toDouble
+      if (aspect < idealAspect) {
+        new java.awt.Rectangle(0, 0, maximal._1, (maximal._2 * aspect).toInt)
+      } else {
+        new java.awt.Rectangle(0, 0, (maximal._1 / aspect).toInt, maximal._2)
+      }
+    }
+
+    // /** 
+    //  * From a real-world envelope and a Rectangle representing the display
+    //  * area, expand the envelope so that it matches the aspect ratio of the
+    //  * desired viewing window.  The center of the envelope is preserved.
+    //  */
+    // def pad(envelope: jts.Envelope, window: java.awt.Rectangle)
+    // : jts.Envelope = {
+    //   val aspect = envelope.getHeight() / 
+    // }
   }
 }
 
