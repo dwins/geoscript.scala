1bba15190ae47ee726f2d60f15f425ca3a37f78e
David Winslow <cdwinslow@gmail.com>
2011-05-14 13:44:25 -0400

    Update Java API implementation to new internal API

--- geocss/src/main/scala/compat/Impl.scala
+++ geocss/src/main/scala/compat/Impl.scala
@@ -1,13 +1,12 @@
 package org.geoscript.geocss
 
 import org.geotools.styling.Style
-import Translator.css2sld
 
 package compat {
   class Impl extends CSS2SLD.Converter {
     override def convert(input: java.io.InputStream): Style =
       CssParser.parse(input) match {
-        case CssParser.Success(style, _) => css2sld(style)
+        case CssParser.Success(style, _) => (new Translator).css2sld(style)
         case (ns: CssParser.NoSuccess) => error(ns.toString)
       }
   }
