8ec83cdceb8ea10b99041a0b291d76821ca0139a
David Winslow <cdwinslow@gmail.com>
2012-02-25 12:52:48 -0500

    Even more porting

--- geocss/src/test/scala/TokenTest.scala
+++ geocss/src/test/scala/TokenTest.scala
@@ -2,50 +2,49 @@ package org.geoscript.geocss
 
 import org.specs2._
 import org.opengis.filter.Filter.{ INCLUDE, EXCLUDE }
+import org.opengis.filter.{ Not, PropertyIsGreaterThan }
 
 /**
  * Tests for token methods
  */
-class TokenTest extends Specification { def is = pending
-//  "'And' Selectors should do some basic simplification" in { 
-//    val filters =
-//      org.geotools.factory.CommonFactoryFinder.getFilterFactory2(null)
-//    val expr1 = ExpressionSelector("a>b")
-//    val expr2 = ExpressionSelector("c<d")
-//
-//    val andA = AndSelector(Nil)
-//    val andB = AndSelector(List(expr1))
-//    val andC = AndSelector(List(expr1, expr2))
-//    andA.filterOpt must beSome(INCLUDE)
-//    andB.filterOpt must beSome(expr1.filterOpt.get)
-//    andC.filterOpt must beSome(
-//      filters.and(expr1.filterOpt.get, expr2.filterOpt.get)
-//    )
-//  }
-//
-//  "'Not' selectors produce negated Filters" in {
-//    AcceptSelector.filterOpt must beSome(INCLUDE)
-//    NotSelector(AcceptSelector).filterOpt must beSome(EXCLUDE)
-//    NotSelector(NotSelector(AcceptSelector)).filterOpt must beSome(INCLUDE)
-//  }
-//
-//  "Spot checks on miscellaneous filter combos" in {
-//    import org.opengis.filter.{PropertyIsGreaterThan,Not}
-//    val expr = ExpressionSelector("a>b")
-//    expr.filterOpt.get must haveSuperClass[PropertyIsGreaterThan]
-//    val not = NotSelector(expr)
-//    not.filterOpt.get must haveSuperClass[Not]
-//    val or = OrSelector(List(expr))
-//    or.filterOpt.get must haveSuperClass[PropertyIsGreaterThan]
-//    val nor = OrSelector(List(not))
-//    nor.filterOpt.get must haveSuperClass[Not]
-//    val and = AndSelector(List(expr))
-//    and.filterOpt.get must haveSuperClass[PropertyIsGreaterThan]
-//    val nand = AndSelector(List(not))
-//    nand.filterOpt.get must haveSuperClass[Not]
-//    val or_and = OrSelector(List(and))
-//    or_and.filterOpt.get must haveSuperClass[PropertyIsGreaterThan]
-//    val or_nand = OrSelector(List(nand))
-//    or_nand.filterOpt.get must haveSuperClass[Not]
-//  }
+class TokenTest extends Specification with matcher.DataTables { 
+  val filterFactory =
+    org.geotools.factory.CommonFactoryFinder.getFilterFactory2(null)
+  import filterFactory._
+
+  val expr1 = ExpressionSelector("a>b")
+  val expr2 = ExpressionSelector("c<d")
+
+  def is = 
+    "'And' Selectors should do some basic simplification" ^ 
+       "empty And goes to Include" ! {
+         AndSelector(Nil).filterOpt must beSome(INCLUDE)
+       } ^ 
+       "single-element And goes to simple filter" ! {
+         AndSelector(List(expr1)).filterOpt must_== expr1.filterOpt
+       } ^
+       "'and' filter is correctly generated for larger And's" ! {
+         AndSelector(List(expr1, expr2)).filterOpt must_== 
+           (for { f <- expr1.filterOpt; g <- expr2.filterOpt }
+             yield and(f, g))
+       } ^ end ^
+    "'Not' selectors produce negated Filters" ! {
+      "selector"                               | "filter"      |
+      NotSelector(AcceptSelector)              ! Some(EXCLUDE) |
+      NotSelector(NotSelector(AcceptSelector)) ! Some(INCLUDE) |
+      AcceptSelector                           ! Some(INCLUDE) |> {
+        (sel, filt) => sel.filterOpt must_== filt
+      }
+    } ^ end ^
+    "Spot checks on miscellaneous filter combos" ! {
+      "selector"                    | "criterion"                           |
+      expr1                         ! beAnInstanceOf[PropertyIsGreaterThan] |
+      NotSelector(expr1)            ! beAnInstanceOf[Not]                   |
+      OrSelector(List(expr1))       ! beAnInstanceOf[PropertyIsGreaterThan] |
+      AndSelector(List(expr1))      ! beAnInstanceOf[PropertyIsGreaterThan] |
+      NotSelector(AndSelector(List(expr1))) ! beAnInstanceOf[Not] |
+      OrSelector(List(AndSelector(List(expr1)))) ! beAnInstanceOf[PropertyIsGreaterThan] |
+      OrSelector(List(AndSelector(List(NotSelector(expr1))))) ! beAnInstanceOf[Not] |>
+      { (sel, pass) => sel.filterOpt must beSome.which(_ must pass) }
+    }
 }
--- geocss/src/test/scala/TranslatorTest.scala
+++ geocss/src/test/scala/TranslatorTest.scala
@@ -2,22 +2,26 @@ package org.geoscript.geocss
 
 import org.specs2._
 
-class TranslatorTest extends Specification { def is = pending
-//  val Translator = new Translator
-//  import Translator.color
-//
-//  def body(x: Translator.OGCExpression): String = {
-//    x match {
-//      case x: org.opengis.filter.expression.Literal =>
-//        x.getValue.toString.toLowerCase
-//      case _ => null
-//    }
-//  }
-//
-//  "Short hexcodes and English names should work as colors" in {
-//    body(color(Literal("#FFAA77"))) must_== ("#ffaa77")
-//    body(color(Function("rgb", List("1.0", "0.665", "0.467") map Literal))) must_== ("#ffaa77")
-//    body(color(Literal("#FA7"))) must_== ("#ffaa77")
-//    body(color(Function("rgb", List("255", "170", "119") map Literal))) must_== ("#ffaa77")
-//  }
+class TranslatorTest extends Specification with matcher.DataTables {
+  val Translator = new Translator
+  import Translator.color
+  
+  def body(x: Translator.OGCExpression): String = {
+    x match {
+      case x: org.opengis.filter.expression.Literal =>
+        x.getValue.toString.toLowerCase
+      case _ => null
+    }
+  }
+
+  def is = 
+    "Short hexcodes and English names should work as colors" ! {
+      "color"            | "hex" |
+      Literal("#FFAA77") ! "#ffaa77" |
+      Function("rgb", List("1.0", "0.665", "0.467") map Literal) ! "#ffaa77" |
+      Literal("#FA7")    ! "#ffaa77" |
+      Function("rgb", List("255", "170", "119") map Literal) ! "#ffaa77" |> {
+        (property, expected) => body(color(property)) must_== expected
+      }
+    }
 }
