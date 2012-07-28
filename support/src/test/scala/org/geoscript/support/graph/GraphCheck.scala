3da1be98b8323c89508e0a7e81fa6113eac9de53
David Winslow <cdwinslow@gmail.com>
2012-07-28 15:22:02 -0400

    Ported GraphCheck to use the Scalatest runner

--- support/src/test/scala/org/geoscript/support/graph/GraphCheck.scala
+++ support/src/test/scala/org/geoscript/support/graph/GraphCheck.scala
@@ -1,37 +1,41 @@
 package org.geoscript.support.graph
 
-import org.scalacheck._, Arbitrary._, Prop._
+import org.scalatest._, prop._
 
-object GraphCheck extends Properties("Maximal cliques") {
+class GraphCheck extends PropSpec with Checkers {
   val parity = (ps: Set[Int]) => 
     (a: Int, b: Int) => (ps - 0).exists(p => (a + b) % p == 0)
 
-  property("are cliques") =
-    forAll { (vs: Set[Int], ps: Set[Int]) =>
+  property("are cliques") {
+    check { (vs: Set[Int], ps: Set[Int]) =>
       val cliques = maximalCliques(vs, parity(ps))
       cliques.forall(isClique(_, parity(ps)))
     }
+  }
 
-  property("are maximal") =
-    forAll { (vs: Set[Int], ps: Set[Int]) =>
+  property("are maximal") {
+    check { (vs: Set[Int], ps: Set[Int]) =>
       val cliques = maximalCliques(vs, parity(ps))
       cliques.forall { c =>
         (vs -- c).forall { v => !isClique(c + v, parity(ps)) }
       }
     }
+  }
 
-  property("contain every node") =
-    forAll { (vs: Set[Int], ps: Set[Int]) =>
+  property("contain every node") {
+    check { (vs: Set[Int], ps: Set[Int]) =>
       val cliques = maximalCliques(vs, parity(ps))
       cliques.flatten == vs
     }
+  }
 
-  property("are never subsets of each other") =
-    forAll { (vs: Set[Int], ps: Set[Int]) =>
+  property("are never subsets of each other") {
+    check { (vs: Set[Int], ps: Set[Int]) =>
       val cliques = maximalCliques(vs, parity(ps))
       cliques.forall { a =>
         (cliques - a).forall { b =>
           !(a subsetOf b) && !(b subsetOf a)
       } }
     }
+  }
 }
