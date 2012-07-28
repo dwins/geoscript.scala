bbadd1151b736b49b232fa042978c08f7fb95db8
David Winslow <cdwinslow@gmail.com>
2012-07-28 14:46:53 -0400

    Port SelectorTest to scalatest

--- geocss/src/test/scala/SelectorTest.scala
+++ geocss/src/test/scala/SelectorTest.scala
@@ -1,10 +1,9 @@
 package org.geoscript.geocss
 
 import org.geotools.filter.text.ecql.ECQL
+import org.scalatest._, matchers._
 
-import org.specs2._, org.specs2.matcher._
-
-class SelectorTest extends Specification with matcher.DataTables {
+class SelectorTest extends FunSuite with ShouldMatchers {
   import Selector.SelectorsAreSentential.{ disprovenBy, provenBy }
 
   def scale_<(s: String): Selector = PseudoSelector("scale", "<", s)
@@ -12,24 +11,19 @@ class SelectorTest extends Specification with matcher.DataTables {
   def not(s: Selector): Selector = Not(s)
   val cql = (ECQL.toFilter(_: String)) andThen (Selector.asSelector)
 
-  def is = 
-    "disproven test" ! {
-      "Givens"          | "Query"           | "Disproven?" |
-      Set(scale_>("1")) ! scale_<("0")      ! true         |
-      Set(scale_>("1")) ! not(scale_>("0")) ! true         |
-      Set(not(scale_>("0"))) ! scale_>("1") ! true         |
-      Set(scale_>("1")) ! scale_>("0")      ! false        |
-      Set(cql("A=1"))   ! cql("A = 2")      ! true         |
-      Set(cql("A=2"))   ! cql("A = 2")      ! false        |> {
-        (known, query, expected) => disprovenBy(known, query) must_== expected
-      }
-    } ^
-    "proven test" ! {
-      "Givens"          | "Query"      | "Proven?" |
-      Set(cql("A=1"))   ! cql("A=2") ! false     |
-      Set(cql("A=2"))   ! cql("A=2") ! true      |
-      Set(scale_>("1")) ! scale_>("0") ! true      |> {
-        (known, query, expected) => provenBy(known, query) must_== expected
-      }
-    }
+  test("disproven test") {
+    assert(disprovenBy(Set(scale_>("1")), scale_<("0")))
+    assert(disprovenBy(Set(scale_>("1")), not(scale_>("0"))))
+    assert(disprovenBy(Set(not(scale_>("0"))), scale_>("1")))
+    assert(disprovenBy(Set(cql("A=1")), cql("A = 2")))
+
+    assert(!disprovenBy(Set(scale_>("1")), scale_>("0")))
+    assert(!disprovenBy(Set(cql("A=2")), cql("A = 2")))
+  }
+
+  test("proven test") {
+    assert(!provenBy(Set(cql("A=1")), cql("A=2")))
+    assert(provenBy(Set(cql("A=2")), cql("A=2")))
+    assert(provenBy(Set(scale_>("1")), scale_>("0")))
+  }
 }
