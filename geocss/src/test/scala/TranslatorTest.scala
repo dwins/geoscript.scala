0896358db062b70eeecfe49a95bd71b137b70655
David Winslow <cdwinslow@gmail.com>
2012-07-28 15:04:02 -0400

    Move TranslatorTest over to Scalatest

--- geocss/src/test/scala/TranslatorTest.scala
+++ geocss/src/test/scala/TranslatorTest.scala
@@ -1,8 +1,8 @@
 package org.geoscript.geocss
 
-import org.specs2._
+import org.scalatest._, matchers._
 
-class TranslatorTest extends Specification with matcher.DataTables {
+class TranslatorTest extends FunSuite with ShouldMatchers {
   val Translator = new Translator
   import Translator.color
   
@@ -14,14 +14,10 @@ class TranslatorTest extends Specification with matcher.DataTables {
     }
   }
 
-  def is = 
-    "Short hexcodes and English names should work as colors" ! {
-      "color"            | "hex" |
-      Literal("#FFAA77") ! "#ffaa77" |
-      Function("rgb", List("1.0", "0.665", "0.467") map Literal) ! "#ffaa77" |
-      Literal("#FA7")    ! "#ffaa77" |
-      Function("rgb", List("255", "170", "119") map Literal) ! "#ffaa77" |> {
-        (property, expected) => body(color(property)) must_== expected
-      }
-    }
+  test("Short hexcodes and English names should work as colors") {
+    body(color(Literal("#FFAA77"))) should equal("#ffaa77")
+    body(color(Function("rgb", List("1.0", "0.665", "0.467") map Literal))) should equal("#ffaa77")
+    body(color(Literal("#FA7"))) should equal("#ffaa77")
+    body(color(Function("rgb", List("255", "170", "119") map Literal))) should equal("#ffaa77")
+  }
 }
