48f1f8308b8a764a0dd83c8e2b8f717528de3b16
David Winslow <cdwinslow@gmail.com>
2012-07-28 21:51:48 -0400

    Port MemorySpec over to Scalatest

--- geoscript/src/test/scala/org/geoscript/workspaces/MemorySpec.scala
+++ geoscript/src/test/scala/org/geoscript/workspaces/MemorySpec.scala
@@ -1,23 +1,21 @@
 package org.geoscript
 package workspace
 
-import org.specs._
+import org.scalatest._, matchers._
 import com.vividsolutions.jts.geom.Point
 
-class MemorySpec extends Specification {
-  "Memory datastores" should {
-    "be able to create layers" in {
-      val schema = feature.Schema("cities",
-        feature.Field("the_geom", classOf[Point], "EPSG:4326"),
-        feature.Field("name", classOf[String])
-      )
-      val ws = workspace.Memory()
-      val lyr = ws.create(schema)
-      lyr += feature.Feature(
-        "the_geom" -> geometry.Point(0, 0),
-        "name" -> "test"
-      )
-      lyr.envelope must not(throwAn[Exception])
-    }
+class MemorySpec extends FunSuite with ShouldMatchers {
+  test("be able to create layers") {
+    val schema = feature.Schema("cities",
+      feature.Field("the_geom", classOf[Point], "EPSG:4326"),
+      feature.Field("name", classOf[String])
+    )
+    val ws = workspace.Memory()
+    val lyr = ws.create(schema)
+    lyr += feature.Feature(
+      "the_geom" -> geometry.Point(0, 0),
+      "name" -> "test"
+    )
+    lyr.envelope should not be(null)
   }
 }
