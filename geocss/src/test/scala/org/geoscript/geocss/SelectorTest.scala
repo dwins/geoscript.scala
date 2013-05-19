7b2d2f1c0795364f1b2677dccfd963e7aecda4b2
David Winslow <cdwinslow@gmail.com>
2013-05-19 12:54:01 -0400

    Mark 0.8.1 release

--- geocss/src/test/scala/org/geoscript/geocss/SelectorTest.scala
+++ geocss/src/test/scala/org/geoscript/geocss/SelectorTest.scala
@@ -6,24 +6,24 @@ import org.scalatest._, matchers._
 class SelectorTest extends FunSuite with ShouldMatchers {
   import Selector.SelectorsAreSentential.{ disprovenBy, provenBy }
 
-  def scale_<(s: String): Selector = PseudoSelector("scale", "<", s)
-  def scale_>(s: String): Selector = PseudoSelector("scale", ">", s)
+  def scale_<(d: Double): Selector = PseudoSelector("scale", "<", d)
+  def scale_>(d: Double): Selector = PseudoSelector("scale", ">", d)
   def not(s: Selector): Selector = Not(s)
   val cql = (ECQL.toFilter(_: String)) andThen (Selector.asSelector)
 
   test("disproven test") {
-    assert(disprovenBy(Set(scale_>("1")), scale_<("0")))
-    assert(disprovenBy(Set(scale_>("1")), not(scale_>("0"))))
-    assert(disprovenBy(Set(not(scale_>("0"))), scale_>("1")))
+    assert(disprovenBy(Set(scale_>(1)), scale_<(0)))
+    assert(disprovenBy(Set(scale_>(1)), not(scale_>(0))))
+    assert(disprovenBy(Set(not(scale_>(0))), scale_>(1)))
     assert(disprovenBy(Set(cql("A=1")), cql("A = 2")))
 
-    assert(!disprovenBy(Set(scale_>("1")), scale_>("0")))
+    assert(!disprovenBy(Set(scale_>(1)), scale_>(0)))
     assert(!disprovenBy(Set(cql("A=2")), cql("A = 2")))
   }
 
   test("proven test") {
     assert(!provenBy(Set(cql("A=1")), cql("A=2")))
     assert(provenBy(Set(cql("A=2")), cql("A=2")))
-    assert(provenBy(Set(scale_>("1")), scale_>("0")))
+    assert(provenBy(Set(scale_>(1)), scale_>(0)))
   }
 }
